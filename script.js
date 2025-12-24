aae71261ad4466d8aa5be1e3766175f23e357a43
sam-hills03 <samhills03@hotmail.com>
2025-12-24 19:43:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// ============================================
-// SUPABASE CONFIGURATION
-// ============================================
 const SUPABASE_URL = 'https://vpfoyxvkkttzlitfajgf.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwZm95eHZra3R0emxpdGZhamdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NDAxMTQsImV4cCI6MjA3NjUxNjExNH0._vyK8s2gXPSu18UqEEWujLU2tAqNZEh3mNwVQcbskxA';
 
