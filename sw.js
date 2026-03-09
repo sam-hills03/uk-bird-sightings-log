f9bfb34cb6543a4c181e65d57bfa13422b40c29b
Uk Bird Log <samhills03@hotmail.com>
2026-03-09 16:13:39 +0000

    PWA 1.1

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'bird-log-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './uk_birds.json',
+  'https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'
+];
+
+// Install the service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve files from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
